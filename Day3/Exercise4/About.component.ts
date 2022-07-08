2fd2dfe62e6360b3fd766a9feff2c51479f0573a
Carlos de Luna Saenz <cdelunasaenz@yahoo.com.mx>
2022-07-08 15:10:49 -0500

    Add files via upload

---
+++ Day3/Exercise4/About.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+   selector:"app-about",
+   templateUrl:"./About.component.html",
+   styleUrls:[]
+})
+
+export class AboutComponent implements OnInit{
+   constructor(){}
+   ngOnInit(): void {
+      
+   }
+}
---
+++ Day3/Exercise4/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { AboutComponent } from './pages/About.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path:"about",
+   component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
